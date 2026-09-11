a5802fe69dab4bec9cf65553a4e1c175f0c29c8f
Mooney0113 <nam011394@yahoo.com>
2026-09-10 21:31:08 -0400

    Adjusted line 1 to line 8

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,10 @@
-﻿Console.WriteLine("Hello World!");
-/*
+﻿/*
 Name: Nicholas Mooney
 Course: CSCI 1250, Section 001
 Assignment: Lab 0, Hello World
 Date: September 10, 2026
 Description: Prints a greeting and short introduction to the console.
 */
+Console.WriteLine("Hello World!");
 Console.WriteLine("My Name is Nicholas Mooney");
 Console.WriteLine("This is my first program at ETSU");
